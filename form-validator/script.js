8f8117c8b6c612d239007176090f363efd09caf6
James McNeil <mcneil_james@hotmail.com>
2020-12-22 17:16:25 +0000

    added form eventlistener

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,1 +1,22 @@
-console.log('working')
+// Grab all the elements from the DOM
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+// functions
+
+const showError = (input, message) => {
+    
+}
+
+// Event listener onSubmit
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if(username.value === ''){
+        showError(username, 'Username is required')
+    } else {
+        showSuccess(username)
+    }
+})
