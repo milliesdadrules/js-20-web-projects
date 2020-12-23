617bb327f20fa9cc74abdb2327e875ff85894990
James McNeil <mcneil_james@hotmail.com>
2020-12-23 11:56:11 +0000

    changed isValidEmail to checkEmail

--- form-validator/script.js
+++ form-validator/script.js
@@ -14,21 +14,24 @@ const showError = (input, message) => {
 }
 
 // Check email is valid
-const isValidEmail = (email) => {
+const checkEmail = (input) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+
+    if (re.test(String(input.value).toLowerCase())) {
+        showSuccess(input)
+    } else {
+        showError(input, `Email is not valid`)
+    }
 }
 
 // Check required fields
 const checkRequired = (inputArray) => {
     inputArray.forEach((input) => {
-
         if(input.value.trim() === ''){
             showError(input, `${getFieldName(input)} is required`)
         } else {
             showSuccess(input)
         }
-        console.log(input.value);
     })
 }
 
@@ -60,4 +63,5 @@ form.addEventListener('submit', (e) => {
     checkRequired([username, email, password, password2])
     checkLength(username, 3, 15)
     checkLength(password, 6, 25)
+    checkEmail(email)
 })
